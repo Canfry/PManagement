18898728c7943b1e35ed3dbbc8d6ea508253f949
fry <fry.land@hotmail.fr>
2023-06-08 14:44:54 +0200

    Register route

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ Session(app)
 connection = sqlite3.connect('projects.db')
 cursor = connection.cursor()
 
-cursor.execute("create TABLE users IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, email TEXT NOT NULL, hash TEXT NOT NULL, position TEXT NOT NULL, team TEXT, PRIMARY KEY (id))")
+cursor.execute("create TABLE users IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, username TEXT NOT NULL, email TEXT NOT NULL, hash TEXT NOT NULL, position TEXT NOT NULL, team TEXT, PRIMARY KEY (id))")
 cursor.execute("create TABLE teams IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE")
 cursor.execute("create TABLE projects IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY(team_id) REFERENCES teams(id) ON DELETE CASCADE ON UPDATE CASCADE")
 
@@ -69,11 +69,46 @@ def login():
         return render_template("login.html")
 
 
-
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    # TODO
-    return render_template('register.html')
+    # Clear session
+    session.clear()
+
+    # Check if method is post
+    if request.method == "POST":
+        # Name, username, email, password, position are required
+        if request.form.get("name") == '' or request.form.get("username") == '' or request.form.get("password") == '' or request.form.get("position") == '':
+            return error('All the fields are required', 403)
+
+        # Check if user exist
+        res = cursor.execute('SELECT username FROM users')
+        usernames = res.fetchall()
+        for username in usernames:
+            if request.form.get('username') == username["username"]:
+                return error('User already exists', 403)
+
+        # Check if password = confirm password
+        if request.form.get("password") != request.form.get("confirmation"):
+            return error("Passwords don't match", 403)
+
+        name = request.form.get("name")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        hash_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        position = request.form.get("position")
+        cursor.execute('INSERT INTO users (name, username, email, password, position) VALUES (?, ?, ?, ?, ?)', (name, username, email, hash_password, position))
+        connection.commit()
+
+        # Query database for username
+        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        return redirect('/')
+
+    else:
+        return render_template('register.html')
 
 
 @app.route("/team")
