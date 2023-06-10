acb8ecdc20985631c642a1b278132af6e9b6c571
fry <fry.land@hotmail.fr>
2023-06-10 18:27:56 +0200

    finishing login + register functionalities

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ from utils import login_required, error
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config.from_object(__name__)
+app.secret_key = 'super secret key'
 Session(app)
 
-connection = sqlite3.connect('projects.db')
+connection = sqlite3.connect('projects.db', check_same_thread=False)
 cursor = connection.cursor()
 
-cursor.execute("create TABLE users IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, username TEXT NOT NULL, email TEXT NOT NULL, hash TEXT NOT NULL, position TEXT NOT NULL, team TEXT, PRIMARY KEY (id))")
-cursor.execute("create TABLE teams IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE")
-cursor.execute("create TABLE projects IF NOT EXISTS (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY(team_id) REFERENCES teams(id) ON DELETE CASCADE ON UPDATE CASCADE")
+cursor.execute("create TABLE IF NOT EXISTS users (id INTEGER NOT NULL, name TEXT NOT NULL, username TEXT NOT NULL, email TEXT NOT NULL, hash TEXT NOT NULL, position TEXT NOT NULL, team TEXT, PRIMARY KEY (id))")
+cursor.execute("create TABLE IF NOT EXISTS teams (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, user_id INTEGER, project_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE)")
+cursor.execute("create TABLE IF NOT EXISTS projects (id INTEGER NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL, status TEXT NOT NULL, team_id INTEGER, FOREIGN KEY(team_id) REFERENCES teams(id) ON DELETE CASCADE ON UPDATE CASCADE)")
 
 
 @app.after_request
@@ -31,14 +34,20 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    # TODO
     return render_template('index.html')
 
 
-@app.route("/login", method=["GET", "POST"])
+@app.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect('/')
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # Forget any user_id
-    session.clear()
+    # session.clear()
 
     # If method = POST
     if request.method == "POST":
@@ -50,19 +59,26 @@ def login():
         elif not request.form.get("password"):
             return error("must provide password", 403)
 
-        # Query database for username
-        res = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-        rows = res.fetchall()
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return error("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
+        # Query database for username and password
+        res = cursor.execute("SELECT username, hash, id FROM users")
+        data = res.fetchall()
+        print(data)
+        for user in range(len(data)):
+            username = data[user][0]
+            # print(data[user][0])
+            # print(data[user][1])
+            # print(data[user][2])
+            if username != request.form.get("username"):
+                return error("invalid username and/or password", 403)
+            elif not check_password_hash(data[user][1], request.form.get("password")):
+                return error("Invalid username and/or password", 403)
+            else:
+                # Remember which user has logged in
+                session["user_id"] = data[user][2]
+                session["user_name"] = username
+                print(session)
+                # Redirect user to home page
+                return redirect("/")
 
         # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -72,7 +88,7 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     # Clear session
-    session.clear()
+    # session.clear()
 
     # Check if method is post
     if request.method == "POST":
@@ -83,8 +99,9 @@ def register():
         # Check if user exist
         res = cursor.execute('SELECT username FROM users')
         usernames = res.fetchall()
-        for username in usernames:
-            if request.form.get('username') == username["username"]:
+        print(usernames)
+        for username in range(len(usernames)):
+            if request.form.get('username') == usernames[username][0]:
                 return error('User already exists', 403)
 
         # Check if password = confirm password
@@ -97,16 +114,18 @@ def register():
         password = request.form.get("password")
         hash_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
         position = request.form.get("position")
-        cursor.execute('INSERT INTO users (name, username, email, password, position) VALUES (?, ?, ?, ?, ?)', (name, username, email, hash_password, position))
+        cursor.execute("INSERT INTO users (name, username, email, hash, position) VALUES (?, ?, ?, ?, ?)", (name, username, email, hash_password, position))
         connection.commit()
 
-        # Query database for username
-        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Query database by username
+        res = cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        user = res.fetchall()
+        print(user)
+
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user[0]
 
         return redirect('/')
-
     else:
         return render_template('register.html')
 
